475d08642494bfe0d0a9090d5d895824e6a203a9
Alice Wagner <awagner@synyx.de>
2025-03-27 10:20:32 +0100

    fixxed_warnings_and_added_singular_for_singular_attempt

--- project.py
+++ project.py
@@ -1,18 +1,22 @@
 import random
 # generate a random int in between 1 and 100
-randomNumber = random.randint(1,100)
+randomNumber = random.randint(1, 100)
 # debug output of random number
-print(randomNumber) 
+print(randomNumber)
 
-attemtps = 0
+attempts = 0
 inputNumber = -1
 while inputNumber != randomNumber:
-    inputNumber=int(input("Please input a Number between 1-100: "))
+    inputNumber = int(input("Please input a Number between 1-100: "))
     if inputNumber < randomNumber:
         print("Too low Cutie 😘")
     if inputNumber > randomNumber:
         print("Too high Cutie 😘")
-    attemtps = attemtps +1
+    attempts = attempts + 1
 print("You guessed the Number Cutie 🥳")
-print("You needed " ,attemtps," attempts.")
-
+if attempts > 1:
+    attempts = str(attempts)
+    print("You needed " + attempts +" attempts.")
+elif attempts == 1:
+    attempts = str(attempts)
+    print("You needed " + attempts +" attempt.")
